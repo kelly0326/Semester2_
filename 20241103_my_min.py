c619c3f827fcf4fb016415229900a92e88dbe1fb
shu liu <shuliu64@yahoo.com>
2024-11-03 09:58:00 -0800

    Initializor + Iteration Pattern

---
+++ 20241103_my_min.py
@@ -0,0 +1,10 @@
+lst = [-2,-5,-8,-7]
+my_min = float("inf")
+for num in lst:
+	if num < my_min
+	my_min = num
+	       # -2
+	       # -5
+	       # -8
+print("list: ", list_)
+print("my sum of this list is: ", my_min)
